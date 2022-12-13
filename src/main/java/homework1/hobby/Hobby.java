888562524b3d324f97d8dda59570e91d79641716
Ivelina111 <kristinevans621@gmail.com>
2022-12-14 01:39:21 +0200

    Initial commit

---
+++ src/main/java/homework1/hobby/Hobby.java
@@ -0,0 +1,22 @@
+package homework1.hobby;
+
+import java.util.Scanner;
+
+public class Hobby {
+    public static void main(String [] args) {
+    System.out.println("Please enter your name:");
+
+    Scanner scanner = new Scanner(System.in);
+
+    String name = scanner.nextLine();
+
+    System.out.println("Please enter your favourite hobby:");
+
+    String hobby = scanner.nextLine();
+
+    System.out.println("Hi, " + name + ", we have useful information about " + hobby + " in our website.");
+
+
+}
+}
+
